08ef3d3c7b5229cd0c5b58ae3d2c549e2cd3097a
RykeVl <iforox@gmail.com>
2020-10-19 22:04:55 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+
+
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined) {
+        return [];
+    }
+
+    let total = [],
+        reverse = false;
+
+    function goDeep(arr) {
+        if (reverse) {
+            for (let i = arr.length - 1; i >= 0; i--) {
+                if (Array.isArray(arr[i])) {
+                    goDeep(arr[i]);
+                    reverse = !reverse;
+                } else {
+                    total.push(arr[i]);
+                }
+            }
+        } else {
+            for (let i = 0; i < arr.length; i++) {
+                if (Array.isArray(arr[i])) {                    
+                    goDeep(arr[i]);
+                    reverse = !reverse;
+                } else {
+                    total.push(arr[i]);
+                }
+            }
+        }
+    }
+
+    goDeep(matrix);
+    return total;
 }
